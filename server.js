ce62daf507fc736a13f3594c3cf06e777b5edab4
sorinies <neoeye2@gmail.com>
2017-08-30 15:21:03 +0900

    add morgan and express-basic-auth

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const morgan = require('morgan');
+const basicAuth = require('express-basic-auth');
+
 const app = express();
 
+app.use(basicAuth({
+    users: { 'admin': 'supersecret' }
+}));
+
 app.set('view engine', 'ejs');
+app.use('/static', express.static('public'));
+app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
   res.render('index.ejs');
