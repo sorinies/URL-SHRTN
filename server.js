a8705b4af2392c3cb7a799ab6cc1ea2aac713843
sorinies <neoeye2@gmail.com>
2017-08-30 16:19:18 +0900

    add short link

--- server.js
+++ server.js
@@ -26,6 +26,17 @@ app.get('/', (req, res) => {
   res.render('index.ejs', {data});
 });
 
+app.get('/:id', (req, res) => {
+  const id = req.params.id;
+  const matched = data.find(item => item.id === id);
+  if (matched) {
+    res.redirect(301, matched.oUrl);
+  } else {
+    res.status(404);
+    res.send('404 Not Found');
+  };
+})
+
 app.listen(3000, () => {
   console.log('listening...')
 });
